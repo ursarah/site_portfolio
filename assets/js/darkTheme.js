ecae0978fc6b797239f8ba370fc810341f7ac57e
ursarah <sarahurcezino@gmail.com>
2023-03-08 17:32:10 -0300

    style: varias mudanças

--- assets/js/darkTheme.js
+++ assets/js/darkTheme.js
@@ -1,6 +1,14 @@
 let btn = document.getElementById("darkTheme")
+let backG = document.getElementById ("bg")
+let img = document.getElementById("image")
 btn.addEventListener("click", btnDark)
 
 function btnDark(){
-    document.getElementById("bg").classList.toggle("active")
+    backG.classList.toggle("active")
+    if (backG.classList.contains("active")) {
+        img.src = "assets/image/darkLight/sol.svg"
+    }
+    else {        
+        img.src = "assets/image/darkLight/lua.svg"
+    }
 }
